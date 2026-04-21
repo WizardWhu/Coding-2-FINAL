d04d2a333bcad06f104eaa9cc6f4a73f583b22ee
WizardWhu <sam.sundue@gmail.com>
2026-04-21 13:08:46 -0400

    Copy and pasted old scripts for ragdoll controller

--- Game Coding 2 FINAL/Assets/Scripts/Character Controllers/CameraController.cs
+++ Game Coding 2 FINAL/Assets/Scripts/Character Controllers/CameraController.cs
@@ -2,15 +2,65 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private Transform[] targetTransforms;
+    private Vector3 target;
+    [SerializeField] private float distanceFromPlayer;
+    [SerializeField] private float buffering;
+    [SerializeField] private float rotationBuffer;
+
+    private Vector3 followTarget;
+
+    [SerializeField] private float cameraMoveSpeed;
+    [SerializeField] private float BaseRotationSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private Vector2 mousePosition;
+    [SerializeField] private float lookSense;
+
+    private void Awake()
     {
-        
+        LargeDinosaurController.movedMouse += OnMouseMove;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void OnMouseMove(Vector2 mousePosition)
+    {
+        if (target != null)
+        {
+            float amountToMove = mousePosition.x * lookSense * Time.deltaTime;
+            transform.RotateAround(new Vector3(target.x, transform.position.y, target.z), Vector3.up, amountToMove);
+        }
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        target = Vector3.zero;
+
+        for (int i = 0; i < targetTransforms.Length; i++)
+        {
+            target += targetTransforms[i].position;
+        }
+        target /= targetTransforms.Length;
+
+
+        followTarget = target + ((transform.position - target).normalized * distanceFromPlayer);
+        if ((followTarget - transform.position).magnitude > buffering)
+        {
+            Vector3 targetMove = (followTarget - transform.position).normalized * cameraMoveSpeed * Time.deltaTime;
+            transform.position += new Vector3(targetMove.x, 0, targetMove.z);
+        }
+
+
+        Quaternion targetRotation = Quaternion.LookRotation(target - transform.position);
+        if (1f - Mathf.Clamp(Quaternion.Dot(transform.rotation, targetRotation), 0, 1) > rotationBuffer / BaseRotationSpeed || (followTarget - transform.position).magnitude > buffering)
+        {
+
+            float rotationSpeed = BaseRotationSpeed * (1 - Mathf.Clamp(Quaternion.Dot(transform.rotation, targetRotation), 0, 1));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
--- Game Coding 2 FINAL/Assets/Scripts/Character Controllers/LargeDinosaurController.cs
+++ Game Coding 2 FINAL/Assets/Scripts/Character Controllers/LargeDinosaurController.cs
@@ -1,16 +1,111 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class LargeDinosaurController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Moves the player through moving the hips
+    private Rigidbody hipsRB;
+
+    [Header("Movement Controls")]
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float strafeSpeed;
+    [SerializeField] private float jumpStrength;
+
+    [Header("Camera")]
+    [SerializeField] private Transform cameraTransform;
+
+    [Header("GroundCheck")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundLayer;
+
+    public static event Action<bool> StartedMoving;
+    public static event Action<Vector3> MoveDirection;
+    private bool isMoving;
+    //Used for the current direction the camera is facing
+
+    private Vector2 lookInput;
+    public static event Action<Vector2> movedMouse;
+    private Vector2 moveInput;
+
+    private bool isGrounded = false;
     void Start()
     {
-        
+        hipsRB = GetComponent<Rigidbody>();
+        StartedMoving?.Invoke(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckGround();
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 nextMove = ((cameraTransform.forward * moveInput.y * forwardSpeed) + (cameraTransform.right * moveInput.x * forwardSpeed));
+
+        if (!isMoving && moveInput.magnitude > 0.1)
+        {
+            isMoving = true;
+            StartedMoving?.Invoke(true);
+        }
+        else if (isMoving && moveInput.magnitude < 0.1)
+        {
+            isMoving = false;
+            StartedMoving?.Invoke(false);
+        }
+
+        if (isMoving) MoveDirection?.Invoke(nextMove.normalized);
+        hipsRB.AddForce(nextMove);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            hipsRB.AddForce(new Vector3(0, jumpStrength, 0), ForceMode.Impulse);
+        }
+    }
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+        movedMouse?.Invoke(lookInput);
+
+    }
+    private void CheckGround()
+    {
+        if (groundCheck == null)
+        {
+            isGrounded = false;
+            return;
+        }
+
+        groundCheck.position = new Vector3(transform.position.x, groundCheck.position.y, transform.position.z);
+
+        isGrounded = Physics.SphereCast(groundCheck.position, groundCheckRadius, Vector3.down,
+            out RaycastHit hit, groundCheckDistance, groundLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 end = groundCheck.position + Vector3.down * groundCheckDistance;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(end, groundCheckRadius);
     }
 }
